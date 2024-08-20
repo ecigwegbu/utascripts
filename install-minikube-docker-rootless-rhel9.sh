b1188c8b3b106edac9d2694017e1ae17b439954e
ecigwegbu <igwegbu@gmail.com>
2024-08-20 14:22:50 -0500

    install-minikube-docker-rootless-rhel9.sh update

--- install-minikube-docker-rootless-rhel9.sh
+++ install-minikube-docker-rootless-rhel9.sh
@@ -2,6 +2,8 @@
 
 # Install minikube using Docker driver on RHEL 9
 # Requires: Docker installed in rootless mode; sudo user; Red Hat registration
+echo -e "\nThis script installs minikube using Docker-rootless driver on RHEL 9"
+echo -e "\n********** Checking pre-requisites..."
 if ! sudo -n true &> /dev/null; then
   echo "User must have sudo without password."
   echo "Exiting..."
@@ -16,19 +18,24 @@ elif ! docker ps &> /dev/null; then
   exit 3  # Docker not installed
 fi
 
+echo -e "\n********** Downloading Minikube..."
 # First download and install the Minikube binary
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 # Start minikube with Docker driver
-dockerd-rootless-setuptool.sh install -f
-docker context use rootless
-minikube start --driver=docker --container-runtime=containerd
-minikube version && minikube status
+# dockerd-rootless-setuptool.sh install -f
+# docker context use rootless
+echo -e "\n********* Starting Minikube..."
+minikube start --driver=docker --container-runtime=containerd && \
+  echo -e "\nMinikube installed...ok" && \
+  minikube version && \
+  minikube status
 
 ## Install kubectl
 # Download the Kubernetes repo:
 # This overwrites any existing configuration in /etc/yum.repos.d/kubernetes.repo
+echo -e "\n********** Downloading the Kubernetes repo..."
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -38,5 +45,13 @@ gpgcheck=1
 gpgkey=https://pkgs.k8s.io/core:/stable:/v1.31/rpm/repodata/repomd.xml.key
 EOF
 # Install kubectl
-sudo yum install -y kubectl
-kubectl --version
+echo -e "\n********** Installing kubectl..."
+sudo yum install -y kubectl && \
+  echo -e "\nKubectl installed...ok" && \
+  kubectl version && \
+  echo -e "\nKubernetes Cluster Node(s):\n" && \
+  kubectl get node -o wide
+echo
+echo                *************** END ****************
+echo 'Minikube-Docker-Rootless-RHEL9 Installer (c) 2024. Unix Training Academy.'
+echo 'Enquiries? dm Author: Elias Igwegbu <igwegbu@gmail.com>'
